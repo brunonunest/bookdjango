1f99553e5ea609f86ec2e6d36d3db947ba84ca9a
brunonunest <brunonunest@gmail.com>
2020-04-13 16:10:12 -0400

    Book catalog working great with CRUD/Permissions done

--- book/catalog/views.py
+++ book/catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -7,3 +7,5 @@ from .serializers import BookSerializer
 class BookView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
