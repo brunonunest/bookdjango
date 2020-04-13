874210b80b34fe8ca7373b90054bf9d1e31c3b0e
brunonunest <brunonunest@gmail.com>
2020-04-13 14:32:09 -0400

    2

--- book/catalog/models.py
+++ book/catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    review = models.CharField(max_length=250)
+
+    class Meta:
+        ordering = ['name']
